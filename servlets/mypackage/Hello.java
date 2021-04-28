0e5d8c4503aed0cfc8f411396d75452db12362b7
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-04-28 22:38:50 +0000

    modified:   servlets/mypackage/Hello.java (add new post request).

--- servlets/mypackage/Hello.java
+++ servlets/mypackage/Hello.java
@@ -8,7 +8,7 @@ public class Hello extends HttpServlet
 {
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException
 	{
-		//res.setContentType("text/html");
+		res.setContentType("text/html");
 		StringBuffer sb = new StringBuffer("<!DOCTYPE html>\n");
 
 		// head
@@ -39,27 +39,39 @@ public class Hello extends HttpServlet
 		sb.append("\t\t\tSpecification-Vendor: Sun Microsystems, Inc.<br>\n");
 		sb.append("\t\t\tSpecification-Version: 4.0<br>\n");
 		sb.append("\t\t</p>\n");
-		sb.append("\t\t<h2>1. Adding Integers</h2>\n");
+		sb.append("\t\t<h2>1. Add Two Integers (GET)</h2>\n");
 		sb.append("\t\t<p>\n");
 
-		// 1. Request: Adding Integers
+		// 1. Request: Add Two Integers (GET)
 		int x = ("".equals(req.getParameter("int_x")) || req.getParameter("int_x") == null)? 0 : Integer.parseInt(req.getParameter("int_x"));
 		int y = ("".equals(req.getParameter("int_y")) || req.getParameter("int_y") == null)? 0 : Integer.parseInt(req.getParameter("int_y"));
 		sb.append("\t\t\t");
 		sb.append("GET method as default: ");
 		sb.append(x + " + " + y + " = ");
 		sb.append(add(x, y));
-		sb.append("<br>\n");
+		sb.append("\n\t\t</p>\n");
 
-		int x1 = ("".equals(req.getParameter("int_x1")) || req.getParameter("int_x1") == null)? 0 : Integer.parseInt(req.getParameter("int_x1"));
-		int y1 = ("".equals(req.getParameter("int_y1")) || req.getParameter("int_y1") == null)? 0 : Integer.parseInt(req.getParameter("int_y1"));
+		sb.append("\t\t<h2>2. Repetition of Think Java (POST)</h2>\n");
+		sb.append("\t\t<p>\n");
+
+		// 2. Request: Repetition of Think Java (POST)
+		String a = ("".equals(req.getParameter("str_a")) || req.getParameter("str_a") == null)? "Hello" : req.getParameter("str_a");
+		String b = ("".equals(req.getParameter("str_b")) || req.getParameter("str_b") == null)? "Hello, World!" : req.getParameter("str_b");
 		sb.append("\t\t\t");
-		sb.append("POST method: ");
-		sb.append(x1 + " + " + y1 + " = ");
-		sb.append(add(x1, y1));
-
-		sb.append("\n");
-		sb.append("\t\t</p>\n");
+		sb.append("POST method.");
+		sb.append("\n\t\t</p>\n");
+		sb.append("\t\t<ol>\n");
+		sb.append("\t\t\t<li>");
+		sb.append("Compile source code.<br><code>javac " + a + ".java</code>");
+		sb.append("</li>\n");
+		sb.append("\t\t\t<li>");
+		sb.append("Execute byte code.<br><code>java " + a + "</code>");
+		sb.append("</li>\n");
+		sb.append("\t\t\t<li>");
+		sb.append("The output is:<br><code>" + b + "</code>");
+		sb.append("</li>\n");
+		sb.append("\t\t</ol>\n");
+		
 		sb.append("\t</body>\n");
 		sb.append("</html>");
 		res.getWriter().println(sb);
