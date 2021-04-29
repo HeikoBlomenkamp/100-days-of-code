90073a5042ed5ad169a22330e657c742d980096f
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-04-29 20:02:28 +0000

    new file:   mikedane/java/t04_drawing_a_shape/App.java (R1D100).

---
+++ mikedane/java/t04_drawing_a_shape/App.java
@@ -0,0 +1,16 @@
+// Title:       Drawing a Shape | Java | Tutorial 4
+// Link:        https://youtu.be/ITOgTeudi1s
+//
+// Modified by: heiko.blomenkamp@gmx.de
+//
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		System.out.println("   /|");
+		System.out.println("  / |");
+		System.out.println(" /  |");
+		System.out.println("/___|");
+	}
+}
