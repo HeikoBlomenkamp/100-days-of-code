e606a241a875e7debaf0ac7bdeecf1744ca5eb7f
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-04-30 21:08:07 +0000

    new file:   mikedane/java/t05_variables/App1.java (R2D1).

---
+++ mikedane/java/t05_variables/App1.java
@@ -0,0 +1,23 @@
+// Title:       Variables - Java - Tutorial 5
+// Link:        https://youtu.be/TVSj_N2Rmfo
+//
+// Modified by: heiko.blomenkamp@gmx.de
+//
+
+public class App1
+{
+	public static void main(String[] args)
+	{
+		//String characterName = "Tom";
+		String characterName = "Dave";
+
+		//int characterAge = 40;
+		int characterAge = 20;
+
+		System.out.println("There once was a man named " + characterName + ",");
+		System.out.println("he was " + characterAge + " years old. He really liked");
+		System.out.println("the name " + characterName + ", but didn't like being " + characterAge + ".");
+		System.out.println("Most of his friends were also " + characterAge + ", and one");
+		System.out.println("of them was also named " + characterName + ".");
+	}
+}
