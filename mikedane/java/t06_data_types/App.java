84a0ab9b73f70b8020670111302009722160b998
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-01 22:56:25 +0000

    new file:   mikedane/java/t06_data_types/App.java (R2D2).

---
+++ mikedane/java/t06_data_types/App.java
@@ -0,0 +1,25 @@
+// Title:       Data Types | Java | Tutorial 6
+// Link:        https://youtu.be/BRtlEMYW0FU
+//
+// Modified by: heiko.blomenkamp@gmx.de
+//
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		String phrase = "To be or not to be";
+		char grade = 'A';
+		int age = 35;
+		double gpa = 2.7;
+		boolean isMale = false;
+
+		System.out.println(phrase);
+		System.out.println(grade);
+		System.out.println(age);
+		System.out.println(gpa);
+		System.out.println(isMale);
+
+		System.out.println("asdfsadf");
+	}
+}
