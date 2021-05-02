74ad17bbd69339b68942b302553d59e86c27d67c
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-02 23:18:51 +0000

    new file: mikedane/java/t07_working_with_strings/App.java (R2D3).

---
+++ mikedane/java/t07_working_with_strings/App.java
@@ -0,0 +1,20 @@
+// Title:       Working With Strings - Java - Tutorial 7
+// Link:        https://youtu.be/yebwyol5M7k
+//
+// Modified by: heiko.blomenkamp@gmx.de
+//
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Giraffe Academy");
+		System.out.println("Giraffe \nAcademy");
+		System.out.println("Giraffe \"Academy\"");
+		System.out.println("Giraffe \\ Academy");
+
+		System.out.println("Giraffe Academy" +
+			"asdfa sadf " +
+			"asdfsadf");
+	}
+}
