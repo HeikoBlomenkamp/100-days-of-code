614f7354f01f45b45aaa72936e22b351645ab07f
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-03 21:41:58 +0000

    new file:   mikedane/java/t08_working_with_math/App.java (R2D4).

---
+++ mikedane/java/t08_working_with_math/App.java
@@ -0,0 +1,19 @@
+// Title:       Working With Math & Numbers - Java - Tutorial 8
+// Link:        https://youtu.be/C3aWV0cTfpY
+//
+// Modified by: heiko.blomenkamp@gmx.de
+//
+
+public class App
+{
+	public static void main(String[] args)
+	{
+		System.out.println( 3 );
+		System.out.println( 3 + 4 );
+		System.out.println( 3 - 4 );
+		System.out.println( 3 * 4 );
+		System.out.println( 3 / 4 );
+		System.out.println( 3.0 / 4.0 );
+		System.out.println( 10 % 3 );
+	}
+}
