63d113b9b0f451c8dd4770b046ccef3d6529d451
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-10 22:36:59 +0200

    new file:   mikedane/php/t04_hello_world/site.php (R2D11).

---
+++ mikedane/php/t04_hello_world/site.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+// Title:       Hello World & Setup | PHP | Tutorial 4
+// Link:        https://youtu.be/7TC0K7aRmFs
+//
+// Modified by: heiko.blomenkamp@gmx.de
+//
+-->
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="description" content="Hello, PHP World!">
+		<title>Document</title>
+	</head>
+	<body>
+		<?php
+			echo ("<h1>Hello, PHP World!</h1>\n");
+		?>
+	</body>
+</html>
