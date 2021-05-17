337290f29b5c9d51247405b9c85a8d63261550d0
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-18 00:11:41 +0200

    new file:   mikedane/php/t05_writing_html/site.php (R2D18).

---
+++ mikedane/php/t05_writing_html/site.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+// Title:       Writing HTML | PHP | Tutorial 5
+// Link:        https://youtu.be/bTzzA0K6Qh4
+//
+// Modified by: heiko.blomenkamp@gmx.de
+//
+-->
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="description" content="Writing HTML.">
+	<title>Document</title>
+</head>
+<body>
+<?php
+	echo "\t<header>\n";
+		echo "\t\t<h1>Writing HTML</h1>\n";
+	echo "\t</header>\n";
+	echo "\t<main>\n";
+		echo "\t\t<hr>\n";
+		echo "\t\t<p>\n";
+			echo "\t\t\tThis is my site.\n";
+		echo "\t\t</p>\n";
+	echo "\t</main>\n";
+?>
+</body>
+</html>
