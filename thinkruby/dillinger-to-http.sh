eb7e31157d36afe18cca029ad3db7a3184904d51
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-24 12:52:03 +0200

    new file:   thinkruby/dillinger-to-http.sh (R2D25).

---
+++ thinkruby/dillinger-to-http.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# $Id: dillinger-to-http.sh,v 2.1 2021/05/24 10:44:19 heiko Exp $
+#
+# $Log: dillinger-to-http.sh,v $
+# Revision 2.1  2021/05/24 10:44:19  heiko
+# R2D25: Initial revision
+#
+
+# Print short help message
+if [ -z $2 ]
+then
+	echo "Usage: $0 <source> <target>"
+	exit 2
+fi
+
+# Array of all headings
+HEADINGS=('Preface' \
+'1_The_way_of_the_program' \
+'11_The_Ruby_programming_language')
+
+# Keep original file
+cp -ua $1 $2
+
+# Remove line number from all anchor related phrases
+for heading in "${HEADINGS[@]}"
+do
+	sed -i -e "s/${heading}_[0-9]\+/${heading}/g" $2
+	if [ $? -eq 0 ]
+	then
+		echo "[  OK  ] $heading"
+	else
+		echo "[FAILED] $heading"
+		exit 2
+	fi
+done
+
+exit 0
