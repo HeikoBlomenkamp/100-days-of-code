334b558a04de2ae6879cdb122579c9c77d26df25
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-27 15:50:12 +0200

    modified:   thinkruby/dillinger-to-http.sh (R2D28).

--- thinkruby/dillinger-to-http.sh
+++ thinkruby/dillinger-to-http.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-# $Id: dillinger-to-http.sh,v 2.1 2021/05/24 10:44:19 heiko Exp $
+# $Id: dillinger-to-http.sh,v 2.2 2021/05/27 13:39:43 heiko Exp $
 #
 # $Log: dillinger-to-http.sh,v $
+# Revision 2.2  2021/05/27 13:39:43  heiko
+# R2D28: Added Section 1.2 (What is a program?).
+#
 # Revision 2.1  2021/05/24 10:44:19  heiko
 # R2D25: Initial revision
 #
@@ -17,7 +20,8 @@ fi
 # Array of all headings
 HEADINGS=('Preface' \
 '1_The_way_of_the_program' \
-'11_The_Ruby_programming_language')
+'11_The_Ruby_programming_language' \
+'12_What_is_a_program')
 
 # Keep original file
 cp -ua $1 $2
