820800f6e04c7ed6cb2995c8a80badad84b942d8
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-29 15:51:51 +0200

    new file:   mikedane/ruby/t05_drawing_a_shape/giraffe.rb (R2D30).

---
+++ mikedane/ruby/t05_drawing_a_shape/giraffe.rb
@@ -0,0 +1,15 @@
+# Title:       Drawing a Shape | Ruby | Tutorial 5
+# Link:        https://youtu.be/-AllFkNLDKk
+#
+# Modified by: heiko.blomenkamp@gmx.de
+
+puts "Ruby"
+puts " is cool!"
+print "Giraffe Academy"
+print " is cool!"
+print "\n\n"
+
+puts "/___|"
+puts "   /|"
+puts "  / |"
+puts " /  |"
