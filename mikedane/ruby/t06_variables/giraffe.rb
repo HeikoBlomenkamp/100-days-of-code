fb4befeac595a7190acd2ec88c06ee98e7fff634
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-29 15:52:34 +0200

    new file:   mikedane/ruby/t06_variables/giraffe.rb (R2D30).

---
+++ mikedane/ruby/t06_variables/giraffe.rb
@@ -0,0 +1,14 @@
+# Title:       Variables | Ruby | Tutorial 6
+# Link:        https://youtu.be/4mWc6Q9shcQ
+#
+# Modified by: heiko.blomenkamp@gmx.de
+
+character_name = "George"
+character_age  = "70"
+character_name = "John"
+character_age  = "35"
+
+puts ("There once was a man named " + character_name + ",")
+puts ("he was " + character_age + " years old.")
+puts ("He really liked the name " + character_name + ",")
+puts ("but he didn't like being " + character_age + ".")
