b99d0cbd68a5537048824ece53dd4a7455418a8b
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-05-29 08:08:08 +0200

    new file:   thinkruby/main/s0131_syntax_errors.rb (R2D30).

---
+++ thinkruby/main/s0131_syntax_errors.rb
@@ -0,0 +1,10 @@
+puts (4 + 5) * 10
+# 90
+
+# puts 8)
+#
+# My error message (if executed):
+#
+# s0131_syntax_errors.rb:4: syntax error, unexpected ')', expecting end-of-input
+# puts 8)
+#       ^
