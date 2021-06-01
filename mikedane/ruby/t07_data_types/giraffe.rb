940df0a69d65ecda935979617b01167ff76611e2
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-06-02 00:49:05 +0200

    new file:   mikedane/ruby/t07_data_types/giraffe.rb (R2D33).

---
+++ mikedane/ruby/t07_data_types/giraffe.rb
@@ -0,0 +1,27 @@
+# Title:       Data Types | Ruby | Tutorial 7
+# Link:        https://youtu.be/rjLB_bD3SLU
+#
+# Modified by: heiko.blomenkamp@gmx.de
+
+name = "Giraffe Academy"        # string
+puts 'Name   = "' + name + '"'
+
+age = 75                        # integer number
+print 'Age    = '
+puts age
+
+gpa = -3.2                      # floating-point number
+print 'GPA    = '
+puts gpa
+
+is_male	= true                  # boolean
+print 'isMale = '
+puts is_male
+
+is_tall	= false                 # boolean
+print 'isTall = '
+puts is_tall
+
+flaws = nil                     # no value			
+print 'Flaws  = '
+puts flaws
