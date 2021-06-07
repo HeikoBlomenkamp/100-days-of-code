51650568142bfed93286bba0af98d5c5e610022e
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-06-07 23:07:36 +0200

    new file:   mikedane/ruby/t09_math_and_numbers/giraffe.rb (R2D39).

---
+++ mikedane/ruby/t09_math_and_numbers/giraffe.rb
@@ -0,0 +1,64 @@
+# Title:       Math & Numbers - Ruby - Tutorial 9
+# Link:        https://youtu.be/qDLYmBm4NF8
+#
+# Modified by: heiko.blomenkamp@gmx.de
+
+print "Out[1]: "
+puts 5
+print "Out[2]: "
+puts 5.86543
+
+print "Out[3]: "
+puts 5 + 9
+print "Out[4]: "
+puts 5 - 9
+print "Out[5]: "
+puts 5 / 9
+print "Out[6]: "
+puts 5 * 9
+
+print "Out[7]: "
+puts 2**3
+print "Out[8]: "
+puts 10 % 3
+
+num = 20
+print "Out[9]: "
+puts num
+print "Out[10]: "
+puts ("My fav num is " + num.to_s)
+
+num = -20
+print "Out[11]: "
+puts num.abs()
+
+num = 20.487
+print "Out[12]: "
+puts num.round()
+
+num = 20.687
+print "Out[13]: "
+puts num.round()
+
+num = 20.1
+print "Out[14]: "
+puts num.ceil()
+
+num = 20.9
+print "Out[15]: "
+puts num.floor()
+
+print "Out[16]: "
+puts Math.sqrt(36)
+print "Out[17]: "
+puts Math.log(1)
+
+print "Out[18]: "
+puts 1 + 7
+print "Out[19]: "
+puts 1.0 + 7
+print "Out[20]: "
+puts 10 / 7
+print "Out[21]: "
+puts 10 / 7.0
+
