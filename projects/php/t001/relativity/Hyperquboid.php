27402406d807b176744d50dc7ad38546e959ea13
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-06-10 19:16:25 +0200

    new file:   projects/php/t001/relativity/Hyperquboid.php (R2D42).

---
+++ projects/php/t001/relativity/Hyperquboid.php
@@ -0,0 +1,55 @@
+<?php
+// $Id: Hyperquboid.php,v 1.1 2021/06/10 16:20:51 heiko Exp $
+//
+// $Log: Hyperquboid.php,v $
+// Revision 1.1  2021/06/10 16:20:51  heiko
+// Initial revision
+//
+
+class Hyperquboid
+{
+	private int $length;
+	private int $width;
+	private int $height;
+	private int $time;
+
+	private function __construct()
+	{
+		$this->length = 1;
+		$this->width  = 1;
+		$this->height = 1;
+		$this->time   = 1;
+	}
+
+	private function getInteriorDiagonal()
+	{
+		return sqrt(
+			$this->length * $this->length +
+			$this->width  * $this->width  +
+			$this->height * $this->height +
+			$this->time   * $this->time
+		);
+	}
+
+	private function getInteriorVolume()
+	{
+		return	$this->length * $this->width *
+			$this->height * $this->time; 
+	}
+
+
+	public static function main(): static
+	{
+		$hq = new static();
+
+		printf("Interior diagonal:\t%1.0f\n",
+			$hq->getInteriorDiagonal());
+		printf("Interior volume:\t%d\n",
+			$hq->getInteriorVolume());
+
+		return $hq;
+	}
+}
+
+Hyperquboid::main();
+?>
