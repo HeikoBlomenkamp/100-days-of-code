c9de54614d11ce07755e57cb7ec9c1ba0e019af4
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-06-12 22:02:36 +0200

    new file:   mikedane/gcc/t04_hello_world/main.c (R2D44).

---
+++ mikedane/gcc/t04_hello_world/main.c
@@ -0,0 +1,14 @@
+/**
+ * Title:       Hello World | C | Tutorial 4
+ * Link:        https://youtu.be/B1CUhnjJ5N4
+ *
+ * Modified by: heiko.blomenkamp@gmx.de
+ */
+
+# include <stdio.h>
+
+int main()
+{
+	printf("hello, world\n");
+	return 0;
+}
