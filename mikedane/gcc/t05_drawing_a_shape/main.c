1f4c7bd8d6c8fe70dd7160830bc522ff08e3d376
Heiko Blomenkamp <heiko.blomenkamp@gmx.de>
2021-06-12 22:03:25 +0200

    new file:   mikedane/gcc/t05_drawing_a_shape/main.c (R2D44).

---
+++ mikedane/gcc/t05_drawing_a_shape/main.c
@@ -0,0 +1,21 @@
+/**
+ * Title:       Drawing a Shape | C | Tutorial 5
+ * Link:        https://youtu.be/iM6WdmDyS-g
+ *
+ * Modified by: heiko.blomenkamp@gmx.de
+ */
+
+# include <stdio.h>
+
+int main()
+{
+	printf("hello, world\n");
+	printf("hello, world\n\n");
+
+	printf("/___|\n");
+	printf("   /|\n");
+	printf("  / |\n");
+	printf(" /  |\n");
+
+	return 0;
+}
